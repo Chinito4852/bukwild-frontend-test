dbf41fcd3e3faeba51226ddaf139e949115c0c72
Chinito4852 <tristinowong@gmail.com>
2021-04-02 22:20:59 -0700

    pre-redo commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import logo from './abc_logo.svg';
+import PageUI from './PageUI';
 import './App.css';
 
-function App() {
+function App(props) {
+
+  const [state, setState] = useState(props.pages[0].title);
+  const [blocks, setBlocks] = useState(props.pages[0].blocks);
+
+  useEffect(() => {
+    //console.log(props.pages);
+    //console.log(state);
+    //console.log(blocks);
+  });
+
+  const renderLayoutUI = () => {
+    return (
+      <div className="layout-ui">
+        <div id="list-logo" >
+          <img src={logo}/>
+          {renderPageList()}
+        </div>
+        <div id="contact">
+          <button className="contact-btn">Contact Us</button>
+        </div>
+      </div>
+    );
+  }
+
+  const renderPageList = () => {
+    return (
+      <div className="pageList">
+        <ul>
+          {props.pages.map((page, key) => {
+            return (
+              <li className={(state === page.title) ? "active" : ""}>{page.title}</li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+
+  const renderPageUI = () => {
+    return (
+      <PageUI block={blocks[0]}/>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {renderLayoutUI()}
+      {renderPageUI()}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import content from './content.json';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App pages={content.pages}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
